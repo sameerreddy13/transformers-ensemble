914a9dc3bcf87775cfe02c1e5c83b2cbcc92b503
Glenn Matlin <glenn.matlin@gmail.com>
2021-11-26 14:54:59 -0500

    delete setup_env.sh

--- bin/setup_env.sh
+++
@@ -1,3 +0,0 @@
-conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
-conda install transformers==4.11.0 tokenizers==0.10.3 datasets -c huggingface
-python -m pip install apache-beam
