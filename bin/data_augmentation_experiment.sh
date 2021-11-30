5e4cdcb4b095b9bf53ce2148d2f334d723a9f6bc
Glenn Matlin <gmatlin3@gatech.edu>
2021-11-29 22:31:30 -0500

    changed experimentation script

--- bin/data_augmentation_experiment.sh
+++ bin/data_augmentation_experiment.sh
@@ -10,7 +10,6 @@ WEIGHT_DECAY=0
 WARMUP_STEPS=1000
 BATCH_SIZE=32
 NUM_EPOCHS=20
-#SESSION_NAME="aug_exp_$1"
 
 RUN_ID="subnet_$1models_lr${LEARNING_RATE}_bs${BATCH_SIZE}_warmup${WARMUP_STEPS}_wd${WEIGHT_DECAY}"
 SAVE_DIR="logs/${DATE}/${RUN_ID}"
@@ -19,9 +18,8 @@ LOG_FILE="${SAVE_DIR}/log.txt"
 mkdir -p "${SAVE_DIR}"
 touch "${LOG_FILE}" && echo "Created ${LOG_FILE}"
 
-CMD="python ensemble_basic.py --augmented --extract-subnetwork --num-models $1 --lr ${LEARNING_RATE} --wd ${WEIGHT_DECAY} --warmup-steps ${WARMUP_STEPS} --num-epochs ${NUM_EPOCHS} --save-dir ${SAVE_DIR}"
-echo "tmux session '${SESSION_NAME}' running command: '$CMD'"
+CMD="python ensemble_basic.py --augmented --extract-subnetwork --num-models $1 --lr ${LEARNING_RATE} --weight-decay ${WEIGHT_DECAY} --warmup-steps ${WARMUP_STEPS} --num-epochs ${NUM_EPOCHS} --save-dir ${SAVE_DIR}"
 
-tmux new-session -d -s "${SESSION_NAME}" "${CMD}" | tee "${LOG_FILE}"
-nohup "$CMD" > "$LOG_FILE" 2>&1 &
+echo "running command: '$CMD'"
+nohup $CMD > $LOG_FILE 2>&1 &
 exit 0
