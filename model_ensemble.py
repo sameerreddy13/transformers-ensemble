a6c84940f98d0a7103751e4472bedb1f88ceb0d0
Sameer S Reddy <sreddy65@mal.cc.gatech.edu>
2021-11-30 21:07:30 -0500

    Implement basic ensemble script

--- model_ensemble.py
+++ model_ensemble.py
@@ -1,18 +1,35 @@
 import torch
-
+import torch.nn.functional as F
 class Ensemble():
     def __init__(self, models, device):
         models = [model.to(device) for model in models] # TODO: split up across gpus    
         self.models = models
         self.device = device
 
+    def fit(self, dataloader):
+        pass
+
+    def predict_batch(self, example):
+        raise NotImplementedError()
+
+    def predict(self, dataloader):
+        '''
+        Return predictions and accuracy for all batches in dataloader
+        '''
+        results = [self.predict_batch(example) for example in dataloader]
+        preds, accs = list(zip(*results))
+        return torch.cat(preds), torch.tensor(accs)
+
 class AverageVote(Ensemble):
+    '''
+    Voting with all models equally weighted
+    '''
     def __init__(self, models, device):
         super().__init__(models, device)
 
     def average_vote(self, input_ids, attention_mask, labels):
         '''
-        Return ensmble predictions and accuracy
+        Return ensemble predictions and accuracy
         '''
         all_preds = []
         for model in self.models:
@@ -23,9 +40,6 @@ class AverageVote(Ensemble):
         preds = torch.stack(all_preds).mode(dim=0).values
         return preds, (preds == labels).float().mean()
 
-    def fit(self, dataloader):
-        pass
-
     def predict_batch(self, example):
         '''
         Return predictions and accuracy on batch
@@ -33,10 +47,37 @@ class AverageVote(Ensemble):
         example = [x.to(self.device) for x in example]
         return self.average_vote(*example)
 
-    def predict(self, dataloader):
-        '''
-        Return predictions and accuracy for all batches in dataloader
-        '''
-        results = [self.predict_batch(example) for example in dataloader]
-        preds, accs = list(zip(*results))
-        return torch.cat(preds), torch.tensor(accs)
+
+class WeightedVote(Ensemble):
+    '''
+    Voting with learned weights per model
+    '''
+    def __init__(self, models, device):
+        super().__init__(models, device)
+        self.w = torch.nn.Parameter(
+            torch.full(len(self))
+            torch.ones(len(self.models), 1, device=device) / len(self.models)
+        )
+
+    def fit(self, dataloader, lr=1e-1):
+        # optimizer = torch.optim.AdamW(lr=)
+        optimizer = torch.optim.SGD([self.w], lr=lr)
+        for example in dataloader:
+            example = [x.to(self.device) for x in example]
+            logits = torch.stack([
+                model(input_ids=example[0], attention_mask=example[1]).logits
+                for model in self.models
+            ]).permute(1, 0, 2)
+            import pdb; pdb.set_trace() 
+            probs = self.w.dot(logits).softmax(-1)
+            labels = example[2]
+            loss = F.cross_entropy(input=probs, target=labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+
+
+        
